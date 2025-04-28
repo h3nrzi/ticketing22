47f02dced72037c7b3828c4eac691d327d8e6bdc
Hossein Rezaei <rezaeig22@gmail.com>
2025-04-28 19:52:03 +0330

    feat: Determining error type

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,15 @@
 import { ErrorRequestHandler } from "express";
+import { RequestValidationError } from "../errors/request-validation";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.log("Something went wrong!", err);
+  if (err instanceof RequestValidationError) {
+    console.log("handling request validation error");
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log("handling database connection error");
+  }
 
   return res.status(400).send({
     message: err.message,
