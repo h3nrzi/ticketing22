1fe01044e4b1e684bb2ecb432fb6c677e4c937e0
Hossein Rezaei <rezaeig22@gmail.com>
2025-04-29 22:30:31 +0330

    feat: Refactor error classes to extend CustomError

---
+++ auth/src/errors/custom-error.ts
@@ -0,0 +1,13 @@
+abstract class CustomError extends Error {
+  constructor(message: string) {
+    super(message);
+
+    // Only because we are extending a built-in class
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  abstract statusCode: number;
+  abstract serializeErrors(): { field?: string; message: string }[];
+}
+
+export default CustomError;
