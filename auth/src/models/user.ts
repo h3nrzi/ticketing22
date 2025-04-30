3c9685346663c97b213024167d07004b18766a5c
Hossein Rezaei <rezaeig22@gmail.com>
2025-04-30 19:55:30 +0330

    feat: define UserAttrs interface and update user model schema

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
+// An interface that describe the properties, that are require to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// Schema for User model
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,5 +18,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// Build User model
 const User = mongoose.model("User", userSchema);
+
+function buildUser(attrs: UserAttrs) {
+  return new User(attrs);
+}
+
 export default User;
