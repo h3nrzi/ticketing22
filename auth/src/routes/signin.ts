2d409c5516f1a92e00f55d3ec9803a919f280822
Hossein Rezaei <rezaeig22@gmail.com>
2025-05-03 11:21:06 +0330

    feat: implement email and password validation in signin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-	return res.send("Hi there!");
-});
+router.post(
+	"/api/users/signin",
+	[
+		body("email").isEmail().withMessage("Email must be valid."),
+		body("password")
+			.trim()
+			.notEmpty()
+			.withMessage("You must supply a password."),
+	],
+	(req: Request, res: Response) => {
+		// handle validation of email and password
+		// if validation fails, throw a RequestValidationError
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
+	}
+);
 
 export { router as signinRouter };
