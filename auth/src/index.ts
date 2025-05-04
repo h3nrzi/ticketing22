5d6ca286d9d3e1302bd2cce7163f1b17a4c5e1c5
Hossein Rezaei <rezaeig22@gmail.com>
2025-05-04 18:50:23 +0330

    refactor: update dependencies

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,11 +7,7 @@ async function startServer() {
 		if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined!");
 
 		// Connect to MongoDB
-		await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-		});
+		await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
 		console.log("Connected to MongoDB!");
 	} catch (err) {
 		console.error(err);
