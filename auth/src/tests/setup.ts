fda3adb593429c5b23343b405c85cc9f515000d9
Hossein Rezaei <rezaeig22@gmail.com>
2025-05-04 20:56:00 +0330

    feat: add Jest setup file for MongoDB in-memory testing

---
+++ auth/src/tests/setup.ts
@@ -0,0 +1,25 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import app from "../app";
+
+let mongo: MongoMemoryServer;
+
+// Connect to MongoDB before all tests
+beforeAll(async () => {
+	mongo = await MongoMemoryServer.create();
+	const mongoUri = mongo.getUri();
+
+	await mongoose.connect(mongoUri);
+});
+
+// Clear the database before each test
+beforeEach(async () => {
+	const collections = await mongoose.connection.db.collections();
+	for (let collection of collections) await collection.deleteMany();
+});
+
+// Close the MongoDB and Mongoose connection after all tests
+afterAll(async () => {
+	if (mongo) await mongo.stop();
+	await mongoose.connection.close();
+});
