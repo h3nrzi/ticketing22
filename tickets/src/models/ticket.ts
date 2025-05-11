d852bb95aad86cbd335d38822ce8ef0d5e2f21fa
Hossein Rezaei <rezaeig22@gmail.com>
2025-05-11 15:25:24 +0330

    feat(tickets): define Ticket schema and model with static build method

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -23,3 +23,45 @@ interface TicketModel extends Model<TicketDoc> {
 //----------------------------------------
 // Schema Definition
 //----------------------------------------
+
+const ticketSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		userId: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		toJSON: {
+			transform(doc, ret) {
+				ret.id = ret._id;
+				delete ret._id;
+				delete ret.version;
+			},
+		},
+	}
+);
+
+//----------------------------------------
+// Schema Methods
+//----------------------------------------
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+	return new Ticket(attrs);
+};
+
+//----------------------------------------
+// Model Creation
+//----------------------------------------
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
