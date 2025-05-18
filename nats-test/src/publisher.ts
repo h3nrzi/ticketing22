52475ad2ae397f5d42a4c562d51f491abc1a56da
h3nrzi <rezaeig22@gmail.com>
2025-05-18 12:08:42 +0330

    feat(nats-test): publish event on NATS connection

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -5,7 +5,19 @@ const stan = nats.connect("ticketing", "abc", {
 	url: "http://localhost:4222",
 });
 
-// listen for events
+// listen for connection
 stan.on("connect", () => {
 	console.log("Publisher connected to NATS");
+
+	// create a data to publish
+	const data = JSON.stringify({
+		id: "123",
+		title: "concert",
+		price: 20,
+	});
+
+	// publish the event
+	stan.publish("ticket:created", data, () => {
+		console.log("Event published");
+	});
 });
