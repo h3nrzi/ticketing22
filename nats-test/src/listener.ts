926693d5cfc96285906aeb41b8aa9c9c4fdb5616
h3nrzi <rezaeig22@gmail.com>
2025-05-19 22:45:13 +0330

    refactor(nats-test): restructure NATS listener to use TicketCreatedListener class for improved message handling

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -31,32 +31,11 @@ stan.on(
 		// log the connection
 		console.log("Listener connected to NATS");
 
-		// set the options for the subscription
-		const options = stan
-			.subscriptionOptions()
-			.setManualAckMode(true) // manually acknowledge the message
-			.setDeliverAllAvailable() // deliver all messages from the beginning
-			.setDurableName("listener-service"); // the name of the durable subscription
-
-		// subscribe to a channel and listen for messages
-		const subscription = stan.subscribe(
-			"ticket:created", // the channel to subscribe to
-			"queue-group-name", // the name of the queue group
-			options // the options for the subscription
-		);
+		// create a new listener
+		const listener = new TicketCreatedListener(stan);
 
 		// listen for messages
-		subscription.on("message", (msg: Message) => {
-			console.log(
-				"Message received:",
-				msg.getSubject(), // the subject of the message
-				msg.getSequence(), // the sequence of the message
-				msg.getData() // the data of the message
-			);
-
-			// acknowledge the message
-			msg.ack();
-		});
+		listener.listen();
 	}
 );
 
@@ -71,46 +50,74 @@ process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
 
 abstract class Listener {
-	abstract subject: string; // the subject of the message
-	abstract queueGroupName: string; // the name of the queue group
-	abstract onMessage(data: any, msg: Message): void; // the callback function that will be called when a message is received
-	private client: Stan; // the client to connect to the NATS server
-	protected ackWait = 5 * 1000; // the time to wait for an ack
+	abstract subject: string;
+	abstract queueGroupName: string;
+	abstract onMessage(data: any, msg: Message): void;
+	protected readonly ackWait = 5 * 1000;
 
-	constructor(client: Stan) {
-		this.client = client;
-	}
+	constructor(private readonly client: Stan) {}
 
-	// set the options for the subscription
+	// ================================
+	// Set the options for the subscription
+	// ================================
+
 	subscriptionOptions() {
 		return this.client
-			.subscriptionOptions()
-			.setDeliverAllAvailable()
-			.setManualAckMode(true)
-			.setAckWait(this.ackWait)
-			.setDurableName(this.queueGroupName);
+			.subscriptionOptions() // get the subscription options
+			.setDeliverAllAvailable() // deliver all messages from the beginning
+			.setManualAckMode(true) // manually acknowledge the message
+			.setAckWait(this.ackWait) // the time to wait for an ack
+			.setDurableName(this.queueGroupName); // the name of the durable subscription
 	}
 
-	// parse the message
+	// ================================
+	// Parse the message
+	// ================================
+
 	parseMessage(msg: Message) {
+		// get the data from the message
 		const data = msg.getData();
+
+		// return the parsed message
 		return typeof data === "string"
 			? JSON.parse(data)
 			: JSON.parse(data.toString("utf8"));
 	}
 
-	// listen for messages
+	// ================================
+	// Listen for messages
+	// ================================
+
 	listen() {
+		// subscribe to the subject
 		const subscription = this.client.subscribe(
 			this.subject,
 			this.queueGroupName,
 			this.subscriptionOptions()
 		);
 
+		// listen for messages
 		subscription.on("message", (msg: Message) => {
 			console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
 			const parsedData = this.parseMessage(msg);
 			this.onMessage(parsedData, msg);
 		});
+	}
+}
+
+class TicketCreatedListener extends Listener {
+	subject = "ticket:created";
+	queueGroupName = "payments-service";
+
+	// ================================
+	// Handle the message
+	// ================================
+
+	onMessage(data: any, msg: Message): void {
+		// log the message
+		console.log("Ticket created:", data);
+
+		// acknowledge the message
+		msg.ack();
 	}
 }
