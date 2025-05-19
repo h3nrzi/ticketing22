155bf5a877598f0515eb75265241e0069a2a405f
h3nrzi <rezaeig22@gmail.com>
2025-05-19 22:15:07 +0330

    feat(nats-test): implement abstract Listener class for NATS message handling

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,5 @@
 import { randomBytes } from "crypto";
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 
 console.clear();
 
@@ -69,3 +69,48 @@ process.on("SIGINT", () => stan.close());
 
 // close the connection when the process is terminated
 process.on("SIGTERM", () => stan.close());
+
+abstract class Listener {
+	abstract subject: string; // the subject of the message
+	abstract queueGroupName: string; // the name of the queue group
+	abstract onMessage(data: any, msg: Message): void; // the callback function that will be called when a message is received
+	private client: Stan; // the client to connect to the NATS server
+	protected ackWait = 5 * 1000; // the time to wait for an ack
+
+	constructor(client: Stan) {
+		this.client = client;
+	}
+
+	// set the options for the subscription
+	subscriptionOptions() {
+		return this.client
+			.subscriptionOptions()
+			.setDeliverAllAvailable()
+			.setManualAckMode(true)
+			.setAckWait(this.ackWait)
+			.setDurableName(this.queueGroupName);
+	}
+
+	// parse the message
+	parseMessage(msg: Message) {
+		const data = msg.getData();
+		return typeof data === "string"
+			? JSON.parse(data)
+			: JSON.parse(data.toString("utf8"));
+	}
+
+	// listen for messages
+	listen() {
+		const subscription = this.client.subscribe(
+			this.subject,
+			this.queueGroupName,
+			this.subscriptionOptions()
+		);
+
+		subscription.on("message", (msg: Message) => {
+			console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+			const parsedData = this.parseMessage(msg);
+			this.onMessage(parsedData, msg);
+		});
+	}
+}
