2d4925d6306de8088f0c9576c7a31f9444f6b8ce
h3nrzi <rezaeig22@gmail.com>
2025-05-19 11:41:48 +0330

    feat(nats-test): add connection close handling and process termination for NATS listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -22,12 +22,19 @@ const stan = nats.connect(
 stan.on(
 	"connect", // the event to listen for
 	() => {
+		// listen for the close event
+		stan.on("close", () => {
+			console.log("NATS connection closed!");
+			process.exit();
+		});
+
+		// log the connection
 		console.log("Listener connected to NATS");
 
 		// set the options for the subscription
 		const options = stan.subscriptionOptions().setManualAckMode(true); // manually acknowledge the message
 
-		// subscribe to a channel
+		// subscribe to a channel and listen for messages
 		const subscription = stan.subscribe(
 			"ticket:created", // the channel to subscribe to
 			"orders-service-queue-group", // the name of the queue group
@@ -48,3 +55,13 @@ stan.on(
 		});
 	}
 );
+
+// ================================
+// Handle SIGINT and SIGTERM
+// ================================
+
+// close the connection when the process is interrupted
+process.on("SIGINT", () => stan.close());
+
+// close the connection when the process is terminated
+process.on("SIGTERM", () => stan.close());
