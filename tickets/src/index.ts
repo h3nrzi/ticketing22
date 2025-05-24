0a7037980f337ca2dadaef56edb86e16c48d8111
h3nrzi <rezaeig22@gmail.com>
2025-05-24 19:50:30 +0330

    feat(tickets): add NATS configuration and environment variable checks for improved service integration

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -2,42 +2,41 @@ import mongoose from "mongoose";
 import app from "./app";
 import { natsWrapper } from "./config/nats-wrapper";
 
-async function startServer() {
+(async () => {
 	try {
-		// Check if JWT_KEY is defined in environment variables
+		// 1. Check if all the required environment variables are defined
 		if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined!");
-
-		// Check if MONGO_URI is defined in environment variables
 		if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined!");
+		if (!process.env.NATS_URL) throw new Error("NATS_URL must be defined!");
+		if (!process.env.NATS_CLUSTER_ID)
+			throw new Error("NATS_CLUSTER_ID must be defined!");
+		if (!process.env.NATS_CLIENT_ID)
+			throw new Error("NATS_CLIENT_ID must be defined!");
 
-		// Connect to NATS
+		// 2. Connect to NATS
 		await natsWrapper.connect(
-			"ticketing",
-			"tickets-service",
-			"http://nats-srv:4222"
+			process.env.NATS_CLUSTER_ID!,
+			process.env.NATS_CLIENT_ID!,
+			process.env.NATS_URL!
 		);
 
-		// Handle NATS connection close event
+		// 3. Handle NATS connection close event
 		natsWrapper.client.on("close", () => {
 			console.log("NATS connection closed!");
 			process.exit();
 		});
 
-		// Handle NATS connection SIGINT and SIGTERM event
+		// 4. Handle NATS connection SIGINT and SIGTERM event
 		process.on("SIGINT", () => natsWrapper.client.close());
 		process.on("SIGTERM", () => natsWrapper.client.close());
 
-		// Connect to MongoDB
+		// 5. Connect to MongoDB
 		await mongoose.connect(process.env.MONGO_URI!);
 		console.log("Connected to MongoDB!");
 	} catch (err) {
 		console.error(err);
 	}
 
-	// Start the server
-	app.listen(3000, () => {
-		console.log("Listening on port 3000!");
-	});
-}
-
-startServer();
+	// 6. Start the server
+	app.listen(3000, () => console.log("Listening on port 3000!"));
+})();
