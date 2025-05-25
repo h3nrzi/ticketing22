274fe7c682874e6a1c1a474900c8b7d0d98079e6
h3nrzi <rezaeig22@gmail.com>
2025-05-25 19:19:58 +0330

    feat(orders): add express-validator for request validation in order routes

--- orders/src/core/order.routes.ts
+++ orders/src/core/order.routes.ts
@@ -1,14 +1,26 @@
+import { requireAuth, validateRequest } from "@h3nrzi-ticket/common";
+import { body } from "express-validator";
+import { Request, Response } from "express";
 import express from "express";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.send("Hello World");
-});
-
-router.post("/", (req, res) => {
-	res.send("Hello World");
-});
+router.post(
+	"/",
+	requireAuth,
+	[
+		body("ticketId")
+			.notEmpty()
+			.withMessage("TicketId must be provided")
+			.custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+			.withMessage("TicketId must be a valid MongoDB ID"),
+		validateRequest,
+	],
+	(req: Request, res: Response) => {
+		res.send("Hello World");
+	}
+);
 
 router.get("/:id", (req, res) => {
 	res.send("Hello World");
