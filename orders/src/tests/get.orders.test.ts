ccb857cc155e71b2991694987483779afc072a42
h3nrzi <rezaeig22@gmail.com>
2025-05-28 15:27:15 +0330

    test(orders): add integration tests for GET /api/orders endpoint, covering authentication and order retrieval scenarios

---
+++ orders/src/tests/get.orders.test.ts
@@ -0,0 +1,45 @@
+import { Ticket } from "../core/entities/ticket.entity";
+import { createOrder, getOrders } from "./helpers/requests";
+
+describe("GET /api/orders", () => {
+	let cookie: string[];
+	let otherCookie: string[];
+
+	beforeAll(() => {
+		cookie = global.signup();
+		otherCookie = global.signup();
+	});
+
+	describe("Authentication", () => {
+		it("returns a 401 if the user is not authenticated", async () => {
+			const res = await getOrders([]);
+			expect(res.status).toBe(401);
+		});
+	});
+
+	describe("Business logics", () => {
+		it("returns an empty array if there are no orders", async () => {
+			const res = await getOrders(cookie);
+			expect(res.body).toEqual([]);
+		});
+
+		it("returns a list of orders", async () => {
+			// Create tickets
+			const ticket1 = await Ticket.create({ title: "concert1", price: 20 });
+			const ticket2 = await Ticket.create({ title: "concert1", price: 20 });
+			const ticket3 = await Ticket.create({ title: "concert2", price: 20 });
+
+			// Create orders for users
+			await createOrder({ ticketId: ticket1.id }, cookie);
+			await createOrder({ ticketId: ticket2.id }, cookie);
+			await createOrder({ ticketId: ticket3.id }, otherCookie);
+
+			// Get the orders
+			const responseForUser1 = await getOrders(cookie);
+			const responseForUser2 = await getOrders(otherCookie);
+
+			expect(responseForUser1.body.length).toEqual(2);
+			expect(responseForUser2.body.length).toEqual(1);
+		});
+	});
+});
--- orders/src/tests/helpers/requests.ts
+++ orders/src/tests/helpers/requests.ts
@@ -5,6 +5,10 @@ interface OrderData {
 	ticketId?: string;
 }
 
+export const getOrders = async (cookie: string[]) => {
+	return request(app).get("/api/orders").set("Cookie", cookie);
+};
+
 export const createOrder = async (data: OrderData, cookie: string[]) => {
 	return request(app).post("/api/orders").set("Cookie", cookie).send(data);
 };
