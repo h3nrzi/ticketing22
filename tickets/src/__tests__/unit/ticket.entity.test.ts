4a747c2c61dee1227bdfe11b035add625b24b4c7
h3nrzi <rezaeig22@gmail.com>
2025-05-30 19:33:44 +0330

    test(tickets): add unit test for optimistic concurrency control in TicketModel

--- tickets/src/__tests__/unit/ticket.entity.test.ts
+++ tickets/src/__tests__/unit/ticket.entity.test.ts
@@ -1,3 +1,28 @@
 import { TicketModel } from "../../core/entities/ticket.entity";
 
-it("implements optimistic concurrency control", async () => {});
+it("implements optimistic concurrency control", async () => {
+	// create an instance of a ticket
+	const ticket = TicketModel.build({
+		title: "concert",
+		price: 5,
+		userId: "123",
+	});
+
+	// save the ticket to the database
+	await ticket.save();
+
+	// fetch the ticket with id "123" and version 0
+	const firstInstance = await TicketModel.findById(ticket.id);
+	const secondInstance = await TicketModel.findById(ticket.id);
+
+	// make two separate changes to the tickets we fetched: id "123" and version 0
+	firstInstance?.set({ price: 10 });
+	secondInstance?.set({ price: 15 });
+
+	// save the first fetched ticket
+	await firstInstance?.save(); // id: "123" and version 0 => id: "123" and version 1
+
+	// save the second fetched ticket and expect an error
+	// because the version is now 1, but the second instance has a version of 0
+	await expect(secondInstance?.save()).rejects.toThrow();
+});
