19cadf343a7030e26a96601b0633d3b01f304a1b
h3nrzi <rezaeig22@gmail.com>
2025-06-09 17:31:08 +0330

    feat(payments): enhance OrderCreatedListener to create and save orders upon event reception

--- payments/src/events/handlers/order-created-listener.ts
+++ payments/src/events/handlers/order-created-listener.ts
@@ -5,10 +5,26 @@ import {
 } from "@h3nrzi-ticket/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../core/entities/order.entity";
 
 export class OrderCreatedListener extends BaseListener<OrderCreatedEvent> {
 	readonly subject: OrderCreatedEvent["subject"] = Subjects.OrderCreated;
-	queueGroupName = queueGroupName;
+	readonly queueGroupName = queueGroupName;
 
-	async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+	async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+		// create a new order
+		const order = Order.build({
+			id: data.id,
+			ticketPrice: data.ticket.price,
+			userId: data.userId,
+			status: data.status,
+			version: data.version,
+		});
+
+		// save the order
+		await order.save();
+
+		// ack the message
+		msg.ack();
+	}
 }
